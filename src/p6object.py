8e0e176ec40eed05987a691e359f6925f60ddedf
flyin01 <globaljm@hotmail.com>
2020-12-04 23:28:31 +0100

    clean the code and prepare for object

---
+++ src/p6object.py
@@ -0,0 +1,7 @@
+# Create an object, keeping the input as our sample data delete the rest
+
+import numpy as np
+# batch of inputs, change name to capital X, standard
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
